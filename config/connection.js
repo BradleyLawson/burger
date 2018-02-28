705958b15d15ec7e07d085f89f28329120eb1579
Brad Lawson <bradlawson@cfl.rr.com>
2018-02-28 17:57:24 -0500

    update to fix connectivity issue

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// setup the code to connect Node to MySQL.
+var mysql = require("mysql");
+
+
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Br@ndn3wm@n",
+    database: "burgers_db"
+  });
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Exporting the connection
+module.exports = connection;
